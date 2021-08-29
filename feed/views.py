cd82c54da343d8b7e5b96878a9028b368c6e4dbe
soohoonkim <soohoon.kim@hotmail.com>
2021-08-29 17:55:39 +0900

    Created templates paths and base and homepage templates

--- feed/views.py
+++ feed/views.py
@@ -6,7 +6,7 @@ from .models import Post
 # Create your views here.
 class HomePageView(ListView):
     http_method_names = ["get"]
-    template_name = "homepage.html"
+    template_name = "feed/homepage.html"
     model = Post
     context_object_name = "posts"
     queryset = Post.objects.all().order_by("-id")[0:30]
