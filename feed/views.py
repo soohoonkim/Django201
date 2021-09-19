25a7765bcf4598c58dd148f72fe88985d27d6341
soohoonkim <soohoon.kim@hotmail.com>
2021-09-19 21:39:48 +0900

    Configured Views to allow users to create new posts as intended and added modal to base.html for creating new posts

--- feed/views.py
+++ feed/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Post
 
@@ -19,7 +20,18 @@ class PostDetailView(DetailView):
     model = Post
     context_object_name = "post"
 
-class CreateNewPost(CreateView):
+class CreateNewPost(LoginRequiredMixin, CreateView):
     model = Post
     template_name = 'feed/create.html'
     fields = ['text']
+    success_url = '/'
+
+    def dispatch(self, request, *args, **kwargs):
+        self.request = request
+        return super().dispatch(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        obj = form.save(commit=False)
+        obj.author = self.request.user
+        obj.save()
+        return super().form_valid(form)
