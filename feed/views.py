85d3726c641e98ba31d674a742a028d97348563a
soohoonkim <soohoon.kim@hotmail.com>
2021-10-01 21:44:12 +0900

    Changed HomePageView to TemplateView to display posts from the people that the user is following

--- feed/views.py
+++ feed/views.py
@@ -1,18 +1,47 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, TemplateView
 from django.views.generic.edit import CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 
+from followers.models import Follower
 from .models import Post
 
 # Create your views here.
-class HomePageView(ListView):
+# ListView setup
+# class HomePageView(ListView):
+#     http_method_names = ["get"]
+#     template_name = "feed/homepage.html"
+#     model = Post
+#     context_object_name = "posts"
+#     queryset = Post.objects.all().order_by("-id")[0:30]
+    # def get_queryset():
+    #     pass
+
+# TemplateView setup
+class HomePageView(TemplateView):
     http_method_names = ["get"]
     template_name = "feed/homepage.html"
-    model = Post
-    context_object_name = "posts"
-    queryset = Post.objects.all().order_by("-id")[0:30]
+
+    def dispatch(self, request, *args, **kwargs):
+        self.request = request
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        if self.request.user.is_authenticated:
+            following = list(
+                Follower.objects.filter(followed_by=self.request.user).values_list('following', flat=True)
+            )
+            print(following)
+            if not following:
+                posts = Post.objects.all().order_by("-id")[0:30]
+            else:
+                posts = Post.objects.filter(author__in=following).order_by("-id")[0:30]
+        else:
+            posts = Post.objects.all().order_by("-id")[0:30]
+        context['posts'] = posts
+        return context
 
 
 class PostDetailView(DetailView):
@@ -20,6 +49,7 @@ class PostDetailView(DetailView):
     template_name = "feed/detail.html"
     model = Post
     context_object_name = "post"
+
 
 class CreateNewPost(LoginRequiredMixin, CreateView):
     model = Post
